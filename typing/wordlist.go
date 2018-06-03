e545885154f6d7046f219d61ebca19b91e00d5aa
yohei okada <okada.yohei@gmail.com>
2018-06-03 10:01:50 +0900

    Add wordlist

---
+++ typing/wordlist.go
@@ -0,0 +1,22 @@
+package typing
+
+import (
+	"strings"
+)
+
+var WordList = strings.Split(englishWordList, "\n")
+
+var englishWordList = `cupcake
+donut
+eclair
+froyo
+gingerbread
+honeycomb
+ice cream sandwich
+jelly bean
+kitkat
+lollipop
+marshmallow
+nougat
+oreo
+`
